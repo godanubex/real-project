78e1734286f18861791510b0a2f9469cd4a06bd0
godanubex <louis.2542@hotmail.com>
2022-07-31 21:51:15 +0700

    My real Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Component/Header';
 import './App.css';
+import Content from './Component/Content';
+import Footer from './Component/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends React.Component
+{
+  
+  render()
+  {
+    
+   
+    return (
+      <div>
+        <Header />
+        <Content /> 
+        <Footer />    
+      </div>
+    
+  
+) } }
+ export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Buy from "./View/Buy/Buy"
+import Sell from "./View/Sell/Sell"
+import Header from './Component/Header';
+import Content from './Component/Content';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="Sell" element={<Sell /> } />
+      <Route path="Buy" element={<Buy />} />
+   {/*   <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+*/} </Routes>
+  </BrowserRouter>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
