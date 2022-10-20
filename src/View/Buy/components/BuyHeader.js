43ea1bba7661f17e7dc98a582001c06f5448a299
godanubex <louis.2542@hotmail.com>
2022-10-20 11:01:54 +0700

    fix port to 80

--- src/View/Buy/components/BuyHeader.js
+++ src/View/Buy/components/BuyHeader.js
@@ -2,7 +2,7 @@ import './BuyHeader.css';
 function BuyHeader(){
     return(
         <header className="Buy-header">
-            <img className="Buy-logo" src="/image/logo.jpg"/>
+
         </header>
 
     )
